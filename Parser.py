bfd450b6cc258fc77872955e22e814bf8d898b48
unknown <mrsigmon@wm.edu>
2024-09-03 14:15:44 -0400

    add working parser

--- Parser.py
+++ Parser.py
@@ -1,39 +1,70 @@
-
-import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+from PIL import Image
+from PIL import ImageDraw
 import os
-import aspose.pdf as ap
+
 
 class Parser:
-    def __init__(self, dest, xml):
-        self.tree = ET.parse(dest)
-        self.root = self.tree.getroot()
-        self.xml = xml
-
-    def parsexml(self, root):
-        #handle parse error
-        for child in root:
-            if len(child.findall("node"))>0:
-                self.parsexml(child)
-            else:
-                child.set('android:background', '@drawable/borderbox')
-        dest = f'output/updated-{self.xml}'
-        #pngdest = f'output/update-{self.xml[0:len(self.xml)-3]}.png'
-        self.tree.write(dest)
-
-        #pdf = ap.Document()
-        #pdf.bind_xml(self.xml)
-        #pdf.save(dest)
-        #self.tree.write(pngdest)
+    def __init__(self, xml):
+        self.inpath = f'Programming-Assignment-Data/{xml}'
+        self.outpath = f'output/updated-{xml}.png'
+        self.tree = self.setsoup(self.inpath)
+        self.tf = False
     
+    def setsoup(self, xml):
+        with open(xml, 'r') as file:
+            tree = BeautifulSoup(file, 'xml')
+            return tree
+    
+    def getboundsformatted(self, leaves):
+        boundlist = []
+        for i in leaves:
+           if (len(i) == 0):
+               bounds = i.get('bounds') #[,][,]
+               tmp = bounds.split('][')
+               firstlist = tmp[0].split(',')
+               secondlist = tmp[1].split(',')
+
+               x1 = firstlist[0][1:]
+               y1 = firstlist[1]
+               x2 = secondlist[0]
+               y2 = secondlist[1][:-1]
+
+               appendlist = []
+               appendlist.append(int(x1))
+               appendlist.append(int(y1))
+               appendlist.append(int(x2))
+               appendlist.append(int(y2))
+
+               boundlist.append(appendlist)
+
+        return boundlist
+               
+    
+    def drawbounds(self, coords):
+        if self.tf == False:
+            tmppng= Image.open(self.xmltopng(self.inpath))
+            self.tf = True
+        else:
+            tmppng = Image.open(self.outpath)
+        draw = ImageDraw.Draw(tmppng)
+        draw.rectangle(coords, fill=None, outline='yellow', width=4)
+        tmppng.save(self.outpath)
+    
+    def xmltopng(self, xml):
+        return f'{xml[:-3]}png'
 
 if __name__ == "__main__":
-    #pip install
+    
     for x in os.listdir("Programming-Assignment-Data"):
-        if x.endswith(".png"):
-            continue
-        elif x == "com.apalon.ringtones.xml":
-            continue
-        else:
-            dest = f'Programming-Assignment-Data/{x}'
-            p = Parser(dest, x)
-            p.parsexml(p.root)
+        
+        if x.endswith(".xml"):
+
+            p = Parser(x)
+            leaves = p.tree.find_all("node")
+            coordinates = p.getboundsformatted(leaves)
+
+            for i in coordinates:
+                p.drawbounds(i)
+            
+            
---
+++ script.sh
@@ -0,0 +1,6 @@
+#Installs xml parsers
+pip install beautifulsoup4
+pip install lxml
+
+#Installs image editing module
+pip install pillow
